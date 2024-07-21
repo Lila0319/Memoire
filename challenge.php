a09dd1479ba5c212754a64707d3878a685320e6c
Lila0319 <septucehouessou@gmail.com>
2024-07-21 19:12:06 +0100

    Message décrivant les modifications

--- challenge.php
+++ challenge.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 require_once 'config.php';
-
+if (!isset($_SESSION['id_user'])) {
+    header('Location: connexion.php');
+    exit;
+}
 $_SESSION['page'] = 'Challenge';
 
 if (!isset($_SESSION['points'])) {
